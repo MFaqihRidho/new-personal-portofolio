83f39072e0de6e7d00015b777c8e34ca7bf5a83f
MFaqihRidho <faqih.fr5@gmail.com>
2022-07-07 20:51:31 +0700

    update projectCard

--- src/Components/Organism/Projects.vue
+++ src/Components/Organism/Projects.vue
@@ -16,8 +16,10 @@ import ProjectCard from "../Molecules/ProjectCard.vue";
     <div class="grid grid-cols-3 gap-y-16 gap-x-20">
       <ProjectCard
         title="Calories Counter"
-        desc="calories adalah blabalablabalbalbalabla"
+        desc="calorie counter web app, if you know this web app is similar to myfitnespal for example, and uses nutritionix as a food data api"
         stack="Vue, Tailwind"
+        liveLink="https://calories-counters.vercel.app"
+        codeLink="https://github.com/MFaqihRidho/calories-counter"
       >
         <template #image>
           <img
@@ -27,9 +29,11 @@ import ProjectCard from "../Molecules/ProjectCard.vue";
           /> </template
       ></ProjectCard>
       <ProjectCard
-        title="Quran Web App Next js Finder"
-        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere odio, amet minus soluta ut quae vel culpa beatae est quis praesentium voluptas incidunt laboriosam ipsa magni mollitia porro saepe doloremque aspernatur ex, aperiam maiores? Vitae rem recusandae neque dolore, nemo libero porro laudantium, molestias a, itaque quam harum quasi nihil!"
-        stack="React, Tailwind"
+        title="Quran Web App Next js"
+        desc="the same as the quran web app on the other side only using next js"
+        stack="Next, Tailwind"
+        liveLink="https://quran-app-nextjs.vercel.app/"
+        codeLink="https://github.com/MFaqihRidho/quran-app-nextjs"
       >
         <template #image>
           <img
@@ -40,8 +44,10 @@ import ProjectCard from "../Molecules/ProjectCard.vue";
       ></ProjectCard>
       <ProjectCard
         title="Quran Web App"
-        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere odio, amet minus soluta ut quae vel culpa beatae est quis praesentium voluptas incidunt laboriosam ipsa magni mollitia porro saepe doloremque aspernatur ex, aperiam maiores? Vitae rem recusandae neque dolore, nemo libero porro laudantium, molestias a, itaque quam harum quasi nihil!"
+        desc="Quran web app with cool ui, can bookmark your verses, and also mark your last reading, may Allah bless you Muslims"
         stack="React, Tailwind"
+        liveLink="https://quran-web-app.vercel.app/"
+        codeLink="https://github.com/MFaqihRidho/quran-app"
       >
         <template #image>
           <img
@@ -52,8 +58,10 @@ import ProjectCard from "../Molecules/ProjectCard.vue";
       ></ProjectCard>
       <ProjectCard
         title="To Do List Matrix Eisenhower Finder"
-        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere odio, amet minus soluta ut quae vel culpa beatae est quis praesentium voluptas incidunt laboriosam ipsa magni mollitia porro saepe doloremque aspernatur ex, aperiam maiores? Vitae rem recusandae neque dolore, nemo libero porro laudantium, molestias a, itaque quam harum quasi nihil!"
-        stack="React, Tailwind"
+        desc="if you don't know i'm using matrix eisenhower so it can increase your productivity.The Eisenhower Matrix is a productivity, prioritization, and time-management framework designed to help you prioritize a list of tasks or agenda items by first categorizing those items according to their urgency and importance."
+        stack="React, Tailwind, Firebase"
+        liveLink="https://to-do-list-matrix.vercel.app/"
+        codeLink="https://github.com/MFaqihRidho/to-do-list"
       >
         <template #image>
           <img
@@ -64,8 +72,10 @@ import ProjectCard from "../Molecules/ProjectCard.vue";
       ></ProjectCard>
       <ProjectCard
         title="Anime Finder"
-        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere odio, amet minus soluta ut quae vel culpa beatae est quis praesentium voluptas incidunt laboriosam ipsa magni mollitia porro saepe doloremque aspernatur ex, aperiam maiores? Vitae rem recusandae neque dolore, nemo libero porro laudantium, molestias a, itaque quam harum quasi nihil!"
-        stack="React, Tailwind"
+        desc="This is an alternative for you who bored with MyAnimelist Ui, so i made this website for you who want to check your favorite anime rating, trailer, review, etc"
+        stack="React, Tailwind, Redux"
+        liveLink="https://animefinder.vercel.app/"
+        codeLink="https://github.com/MFaqihRidho/animedb-website"
       >
         <template #image>
           <img
@@ -76,8 +86,10 @@ import ProjectCard from "../Molecules/ProjectCard.vue";
       ></ProjectCard>
       <ProjectCard
         title="Movie Finder"
-        desc="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere odio, amet minus soluta ut quae vel culpa beatae est quis praesentium voluptas incidunt laboriosam ipsa magni mollitia porro saepe doloremque aspernatur ex, aperiam maiores? Vitae rem recusandae neque dolore, nemo libero porro laudantium, molestias a, itaque quam harum quasi nihil!"
+        desc="This is an alternative for you who bored with IMDB Ui, so i made this website for you who want to check your favorite Movie rating, trailer, etc"
         stack="React, Tailwind"
+        liveLink="https://movie-finder-website.vercel.app/"
+        codeLink="https://github.com/MFaqihRidho/moviedb-website"
       >
         <template #image>
           <img
